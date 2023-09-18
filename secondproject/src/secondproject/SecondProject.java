15101fb5d6be7d743c55653e6e89bcfc7e9b4aa3
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2023-09-18 14:16:38 -0500

    Made java project and added print line

---
+++ secondproject/src/secondproject/SecondProject.java
@@ -0,0 +1,10 @@
+package secondproject;
+
+public class SecondProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my second prokect that is linked to a git repo");
+	}
+
+}
