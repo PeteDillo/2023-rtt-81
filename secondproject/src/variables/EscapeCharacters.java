08f86d2006ae6e028d46fdffa8489839aeb3207c
Pedro Delgadillo <Pedelgadillo@gmail.com>
2023-09-19 13:12:54 -0500

    added code that was left unstaged

---
+++ secondproject/src/variables/EscapeCharacters.java
@@ -0,0 +1,24 @@
+package variables;
+
+public class EscapeCharacters {
+
+	public static void main(String[] args) {
+		//this is an example of the tab escape character /t
+		System.out.println("This is a tab character ->\t<-");
+		
+		//basic formating with tabs
+		System.out.println("1\tOne\t\tCol3");
+		System.out.println("10\tTen\t\tCol3");
+		System.out.println("100\tOne Hundred\tCol3");
+		//this is an example of a new line character \n
+		System.out.println("This is line 1\nThis is line 2");
+		System.out.println("This is line 3");
+		System.out.println("This is line 4");
+		
+		// \\
+		System.out.println("Backlash \\");
+		// \"
+		System.out.println("\"Quotes\"");
+	}
+
+}
